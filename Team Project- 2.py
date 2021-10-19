194c857e3021e099126705b14ef0c8d2554f78d9
akoschnitzki <91989133+akoschnitzki@users.noreply.github.com>
2021-10-19 16:06:49 -0500

    Add files via upload
    In this file, we showed a program that was able to help print a statement when you put a number in the program.

---
+++ Team Project- 2.py
@@ -0,0 +1,13 @@
+option = input("Enter your Option")
+if option == "1":
+    print("Learn to use Python")
+elif option == "2":
+    print("Go Swimming")
+elif option == "3":
+    print("Having Dinner")
+elif option == "4":
+    print("Go to bed")
+elif option == "5":
+    print("Time to eat")
+else:
+    print("Learn to use Java")
